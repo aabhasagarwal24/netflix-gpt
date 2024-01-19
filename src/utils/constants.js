f7dcf5f608505b514b3c8f47181dc0df81ecfe22
aabhasagarwal24 <aabhasagarwal1234@gmail.com>
2024-01-19 17:08:55 +0530

    TMBD API data fetch && built MainContainer

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTBmZTRhNDI1YWZlYjAwOTI3ZjZkYTZmY2JiNGQ4YSIsInN1YiI6IjY1YTkyZTQ4MGU1YWJhMDEyYzdkZWY4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xPE53VHkJlv7A9gVHxH_loOyCbLIKBilLA3yzwMWr88'
+  },
+};
